e86945b00d6b35d59f098bd5b88c265fc931e31a
leoncito84 <leoncito@live.com>
2017-08-27 12:00:29 +0100

    Applicatio name

---
+++ NHS_Accounts2/NHS_Accounts2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NHS_Accounts2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ NHS_Accounts2/NHS_Accounts2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NHS_Accounts2.MvcApplication" Language="C#" %>
